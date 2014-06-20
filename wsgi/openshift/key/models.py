809a54e0539d2e1b46b42a790f42e90b3cbf2bed
Matt Ziegler <mattpziegler@gmail.com>
2014-06-20 16:17:31 -0500

    added models, created key app

---
+++ wsgi/openshift/key/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Key(models.Model):
+  name = models.CharField(max_length=128)
+  description = models.TextField()    
+  # TODO link to user and/or group? Create an auth group for each key?
+  # last-modified timestamp?
+ 
+class Question(models.Model):
+  key = models.ForeignKey(Key)
+  shortname = models.CharField(max_length=40)
+  text = models.TextField()
+  trueanswer = models.TextField()
+  falseanswer = models.TextField()
+  taxa = models.ManyToManyField(Taxon, through='Question_Taxon')
+  # link to pictures?
+  # user?
+  # timestamp?
+  
+class Taxon(models.Model):
+  key = models.ForeignKey(Key)
+  name = models.CharField(max_length=256)
+  TOLwebID = models.PositiveIntegerField(null=True) #optional
+  description = models.TextField()
+  # timestamp?
+  # user?
+  
+class Question_Taxon(models.Model):
+  question = models.ForeignKey(Question)
+  taxon = models.ForeignKey(Taxon)
+  answer = models.BooleanField()
+  # timestamp?
+  # user?

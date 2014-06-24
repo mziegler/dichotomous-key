c7208eb2dfd85a37b59c2653d2add7ef4b942cb0
Matt Ziegler <mattpziegler@gmail.com>
2014-06-23 22:18:08 -0500

    trying to get css back for admin app, re-arranged static files again

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -105,6 +105,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = ''
-STATICFILES_DIRS = (os.path.normpath(os.path.join(BASE_DIR, '..', 'static')),)
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'

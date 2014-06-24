afd6d70e5e653520cebc4c0728cf883ef82cda8c
Matt Ziegler <mattpziegler@gmail.com>
2014-06-23 21:47:55 -0500

    think I got static files working?  about to find out

--- setup.py
+++ setup.py
@@ -6,15 +6,11 @@ import os
 # Uncomment one or more lines below in the install_requires section
 # for the specific client drivers/modules your application needs.
 packages = ['Django<=1.6',
-            'static3',  # If you want serve the static files in the same server
+             'static3',  # If you want serve the static files in the same server
             # 'mysql-connector-python',
             # 'pymongo',
             # 'psycopg2',
            ]
-
-if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
-     packages.append('django-redis-cache')
-     packages.append('hiredis')
 
 setup(name='Dichotmous Key', version='1.0',
       description='OpenShift Python-3.3 / Django-1.6 Community Cartridge based application',
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -9,10 +9,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'views.home', name='home'),
+    url(r'^$', 'views.home', name='home'),
     # url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-# serves static files only when debug is turned on
+) 

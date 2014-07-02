4eed94af43ec0c6d87392758b92c9b7029516b1c
Matt Ziegler <mattpziegler@gmail.com>
2014-07-01 22:15:58 -0500

    finished code to update key state (by answering or un-answering a question

--- wsgi/openshift/key/static/keydriver.js
+++ wsgi/openshift/key/static/keydriver.js
@@ -3,9 +3,10 @@
 // "keyID" variable declared above script
 
 
+// a JSON object that keeps track of this users session with the key
 var state = {
-  "keyID" : keyID,
-  "useranswers": {}, // dict of (questionID, answer) pairs the user has provided
+  "keyID" : keyID, // which key is currently being used
+  "useranswers": {}, // dict of (questionID, answer) pairs the user has answered so far
   "remainingtaxa": [], // list of remaining taxon ID's
   "eliminatedtaxa": [], // list of eliminated taxon ID's
   "action": [], // tuple describing state modification for the server to do
@@ -16,14 +17,15 @@ var state = {
 // And looking up remaining taxa and eliminated taxa
 function updateState() 
 {
+  $("body").append("<br />request: " + JSON.stringify(state));
   $.post("updatestate", JSON.stringify(state))
   .done( function(data)
   {
     
-    alert(data);
+    $("body").append("<br />response: " + data);
     state = $.parseJSON(data);
     
-    // TODO: call routines for updating interface  
+    updateViews();
   })
   .fail( function()
   {
@@ -31,4 +33,34 @@ function updateState()
   });
 }
 
-$(document).ready(updateState);
+// update the parts of the interface that depend on the key state
+function updateViews()
+{
+  //TODO
+}
+
+function answerTrue(questionID)
+{
+  state.action = ['answer', questionID, true];
+  updateState();
+}
+
+function answerFalse(questionID)
+{
+  state.action = ['answer', questionID, false];
+  updateState();
+}
+
+function removeAnswer(questionID)
+{
+  state.action = ['removeanswer', questionID];
+  updateState();
+}
+
+$(document).ready( function()
+{
+  updateState();
+  answerTrue(1);
+  answerFalse(2);
+  removeAnswer(1);
+});
--- wsgi/openshift/key/views.py
+++ wsgi/openshift/key/views.py
@@ -8,23 +8,65 @@ from django.views.decorators.csrf import csrf_exempt
 import key.keylogic as keylogic
 from key.models import Key
 
-# Create your views here.
 
-"""Parse JSON out from the POST request body, applying the action if there is one,
- into a KeyState object"""
-def getstate(request):
-  #data = json.loads(request.body)
+
+"""Parse JSON out from the POST request body"""
+def loadstateJSON(request):
   s = request.body.decode(encoding='UTF-8')
-  data = json.loads(s)
-  return data
-  #TODO create KeyState
-  
+  return json.loads(s)
+
+
+
+"""Take a state (key-out session) from the user as JSON, apply the action if it 
+exists, look up which taxa are left from the database, and return an HTTP 
+response with the new JSON state."""
 @csrf_exempt
 def updatestate(request):
-  #state = getstate(request)
-  #return getstate(request)
-  #raise Http404
-  return HttpResponse(repr(getstate(request)))
+  
+  instate = loadstateJSON(request)
+  
+  state = None
+  try:
+    state = keylogic.KeyState(int(instate.get('keyID')))
+  except (ValueError, TypeError):
+    return HttpResponse('keyID is required and must be an integer.', status=400)
+  
+  try:
+    state.answers = {int(k): v for (k,v) in instate.get('useranswers').items()}
+  except ValueError:
+    return HttpResponse('Question ID\'s must be integers (or strings parsable into integers)', status=400)
+  except AttributeError:
+    pass # no 'useranswers' was provided (probably), do nothing
+  
+  # apply action (eg. the user answered a question)
+  if instate.get('action'):
+    try:
+      action = instate['action']
+      if action[0] == 'answer':
+        state.answer(int(action[1]), bool(action[2]))
+      elif action[0] == 'removeanswer':
+        state.remove(int(action[1]))
+    except (IndexError, ValueError):
+      return HttpResponse('Bad action.  Valid options are [\'answer\', (integer) questionID, (boolean) value] and [\'removeanswer\', (integer) questionID].', status=400)
+  
+  # look up remaining and eliminated taxa
+  eliminatedtaxa = keylogic.eliminatedtaxa(state)
+  remainingtaxa = keylogic.remainingtaxa(state, eliminatedtaxa)
+
+  # package up data into JSON response
+  response = HttpResponse()
+  s = json.dumps({
+    'keyID': state.keyID,
+    'useranswers': {str(k): v for k,v in state.answers.items()},  
+    'action': [],
+    'remainingtaxa': [r.id for r in remainingtaxa],
+    'eliminatedtaxa': [e.id for e in eliminatedtaxa],
+    })
+  print ('response', s)
+  response.write(s)
+  return response
+  
+
 
 """Render and return the top-level page for a key"""  
 @csrf_exempt
